def38cffd9ec2405473669fe84250a975443212f
Flurin Rindisbacher <info@flurischt.ch>
2019-02-10 17:09:58 +0100

    add frontend

---
+++ README.md
@@ -0,0 +1,8 @@
+# RPI-Remote
+A small webapp to control a raspberry pi.
+
+## Development
+```
+./node.sh /bin/bash -c "npm install && ./node_modules/gulp/bin/gulp.js"
+firefox http://localhost:3000/
+```
---
+++ node.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pushd "$(dirname "$0")"
+
+sudo docker run \
+    --user node \
+    --rm -it \
+    -v `pwd`:/home/node/src \
+    -p 3000:3000 \
+    -w /home/node/src \
+    node:lts \
+    "$@"
+
+popd
